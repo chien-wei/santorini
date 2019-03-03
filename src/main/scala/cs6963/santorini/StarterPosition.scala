fc338164b0e6ac513adede3751dd72e680bd5663
chien-wei <chien-wei@outlook.com>
2019-03-03 10:18:37 -0700

    handle player2 starter position

--- src/main/scala/cs6963/santorini/StarterPosition.scala
+++ src/main/scala/cs6963/santorini/StarterPosition.scala
@@ -2,26 +2,39 @@ package cs6963.santorini
 
 import scala.util.Random.nextInt
 
+// TODO: There should be a better way to do this.
 trait StarterPosition[A, B] {
-  def random(player1: A, player2: A): (A, B)
+  def random1(player1: A, player2: A): (A, B)
+  def random2(player1: A, player2: B): (B, B)
 }
 
 object StarterPositionInstances {
   implicit val prePre: StarterPosition[PrePlayer, Player] = new StarterPosition[PrePlayer, Player] {
-    override def random(player1: PrePlayer, player2: PrePlayer): (PrePlayer, Player) = {
+    override def random1(player1: PrePlayer, player2: PrePlayer): (PrePlayer, Player) = {
       val token1 = List(nextInt(3) + 2, nextInt(3) + 2)
       var token2 = List(nextInt(3) + 2, nextInt(3) + 2)
       while (token1 == token2) {
         token2 = List(nextInt(3) + 2, nextInt(3) + 2)
       }
-      //println((player2, Player(List(token1, token2), player1.card)))
+      (player2, Player(List(token1, token2), player1.card))
+    }
+
+    override def random2(player1: PrePlayer, player2: Player): (Player, Player) = {
+      val token1 = List(nextInt(3) + 2, nextInt(3) + 2)
+      var token2 = List(nextInt(3) + 2, nextInt(3) + 2)
+      while (token1 == token2) {
+        token2 = List(nextInt(3) + 2, nextInt(3) + 2)
+      }
       (player2, Player(List(token1, token2), player1.card))
     }
   }
 }
 
 object StarterPosition {
-  def random[A, B](player1: A, player2: A)(implicit starterPosition: StarterPosition[A, B]) = {
-    starterPosition.random(player1, player2)
+  def random1[A, B](player1: A, player2: A)(implicit starterPosition: StarterPosition[A, B]) = {
+    starterPosition.random1(player1, player2)
+  }
+  def random2[A, B](player1: A, player2: B)(implicit starterPosition: StarterPosition[A, B]) = {
+    starterPosition.random2(player1, player2)
   }
 }
--- src/test/scala/cs6963/santorini/StarterPositionTest.scala
+++ src/test/scala/cs6963/santorini/StarterPositionTest.scala
@@ -4,9 +4,15 @@ import org.scalatest.{FlatSpec, Matchers}
 class StarterPositionTest extends FlatSpec with Matchers {
   import StarterPositionInstances._
   "StarterPosition.random" should "generate random position from 2 PrePlayer" in {
-    val players = StarterPosition.random[PrePlayer, Player](PrePlayer("Nocard"), PrePlayer("Nocard"))
-    println(players)
+    val players = StarterPosition.random1[PrePlayer, Player](PrePlayer("Nocard"), PrePlayer("Nocard"))
     players._1 shouldBe PrePlayer("Nocard")
+    players._2.card shouldBe "Nocard"
+    players._2.tokens.flatten.filter(y => y > 4 || y < 2) shouldBe List()
+  }
+
+  "StarterPosition.random" should "generate random position from 1 PrePlayer and 1 Player" in {
+    val players = StarterPosition.random2[PrePlayer, Player](PrePlayer("Nocard"), Player(List(List(2, 1), List(3, 5)), "Nocard"))
+    players._1 shouldBe Player(List(List(2, 1), List(3, 5)), "Nocard")
     players._2.card shouldBe "Nocard"
     players._2.tokens.flatten.filter(y => y > 4 || y < 2) shouldBe List()
   }
