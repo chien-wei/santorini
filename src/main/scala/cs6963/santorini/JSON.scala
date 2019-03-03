838915f69839c851a04c34b8dd65a94f1a77627c
chien-wei <chien-wei@outlook.com>
2019-03-03 08:02:22 -0700

    add nocard player parser and testcase

--- src/main/scala/cs6963/santorini/JSON.scala
+++ src/main/scala/cs6963/santorini/JSON.scala
@@ -5,7 +5,7 @@ import io.circe.generic.auto._
 //case class Player(tokens: List[List[Int]], card: String)
 //case class Spaces(spaces: List[List[Int]])
 
-object JSON{
+object JSON {
   def parseJSON (input: String): Board = {
 
     val json: Json = parser.parse(input).getOrElse(Json.Null)
@@ -28,9 +28,50 @@ object JSON{
       case Left(error) => println(error.getMessage())
     }
 
-    //println(players)
-    //println(turn, spaces)
     Board(turn.toString.toInt, players.asInstanceOf[List[Player]], spaces.asInstanceOf[List[List[Int]]])
   }
 
+  def parsePre (input: String)= {
+    val json: Json = parser.parse(input).getOrElse(Json.Null)
+    val cursor: HCursor = json.hcursor
+    val prePlayerEither = cursor.as[(PrePlayer, Player)]
+    val prePlayers = prePlayerEither match {
+      case Right(s) => s
+      case Left(error) => {
+        val prePlayerEither2 = cursor.as[(PrePlayer, PrePlayer)]
+        val prePlayers2 = prePlayerEither2 match {
+          case Right(s) => s
+          case Left(error2) => {
+            System.err.println("Playing with no card...")
+            val noCardPlayerEither = cursor.as[List[List[List[Int]]]]
+            val noCardPlayer = noCardPlayerEither match {
+              case Right(s) => {
+                s match {
+                  case List() => (PrePlayer("Nocard"), PrePlayer("Nocard"))
+                  case List(xs) => (PrePlayer("Nocard"), Player(xs, "Nocard"))
+                }
+              }
+              case Left(error3) => {
+                println("Error: Invalid input.")
+              }
+            }
+            noCardPlayer
+          }
+        }
+        prePlayers2
+      }
+    }
+    prePlayers
+  }
+
+}
+
+object tmp2 extends App {
+  val json1 = JSON.parsePre("""[]""")
+  val json2 = JSON.parsePre("""[[[1,5],[2,2]]]""")
+  val json3 = JSON.parsePre("""[{"card":"Artemis"},{"card":"Prometheus"}]""")
+  println(json1)
+  println(json2)
+  println(json3)
+  println((PrePlayer("Nocard"), PrePlayer("Nocard")))
 }
--- src/test/scala/cs6963/santorini/JSONTest.scala
+++ src/test/scala/cs6963/santorini/JSONTest.scala
@@ -2,10 +2,33 @@ package cs6963.santorini
 import org.scalatest.{FlatSpec, Matchers}
 
 class JSONTest extends FlatSpec with Matchers {
-  "JSON parser" should "parse valid string" in {
+  "JSON.parseJSON" should "parse valid string" in {
     val board = JSON.parseJSON("""{"turn":18,"players":[{"tokens":[[2,3],[4,4]],"card":"Artemis"},{"tokens":[[2,5],[3,5]],"card":"Prometheus"}],"spaces":[[0,0,0,0,2],[1,1,2,0,0],[1,0,0,3,0],[0,0,3,0,0],[0,0,0,1,4]]}""")
     board.turn shouldBe 18
     board.spaces shouldBe List(List(0, 0, 0, 0, 2), List(1, 1, 2, 0, 0), List(1, 0, 0, 3, 0), List(0, 0, 3, 0, 0), List(0, 0, 0, 1, 4))
     board.players shouldBe List(Player(List(List(2, 3), List(4, 4)),"Artemis"), Player(List(List(2, 5), List(3, 5)),"Prometheus"))
   }
+
+  "JSON.parsePre" should "parse valid first no-card setup string" in {
+    val preplayers = JSON.parsePre("""[]""")
+    preplayers shouldBe (PrePlayer("Nocard"), PrePlayer("Nocard"))
+  }
+
+  "JSON.parsePre" should "parse valid second no-card setup string" in {
+    val preplayers = JSON.parsePre("""[[[1,5],[2,2]]]""")
+    preplayers shouldBe (PrePlayer("Nocard"), Player(List(List(1, 5), List(2, 2)),"Nocard"))
+  }
+
+  "JSON.parsePre" should "parse valid first setup string" in {
+    val preplayers = JSON.parsePre("""[{"card":"Artemis"},{"card":"Prometheus"}]""")
+    preplayers shouldBe (PrePlayer("Artemis"), PrePlayer("Prometheus"))
+  }
+
+  "JSON.parsePre" should "parse valid second setup string" in {
+    val preplayers = JSON.parsePre("""[{"card":"Prometheus"},{"tokens":[[2,3],[4,4]],"card":"Artemis"}]""")
+    preplayers shouldBe (PrePlayer("Prometheus"), Player(List(List(2, 3), List(4, 4)),"Artemis"))
+  }
+
+
+
 }
