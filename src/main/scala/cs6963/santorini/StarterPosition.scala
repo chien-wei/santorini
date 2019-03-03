e8e803d61dc427d0ca4e02df9e771adccb0d682f
chien-wei <chien-wei@outlook.com>
2019-03-03 10:32:52 -0700

    use DummyImplicit!

--- src/main/scala/cs6963/santorini/StarterPosition.scala
+++ src/main/scala/cs6963/santorini/StarterPosition.scala
@@ -4,37 +4,26 @@ import scala.util.Random.nextInt
 
 // TODO: There should be a better way to do this.
 trait StarterPosition[A, B] {
-  def random1(player1: A, player2: A): (A, B)
-  def random2(player1: A, player2: B): (B, B)
+  def random(player1: A, player2: A): (A, B)
+  def random(player1: A, player2: B)(implicit di: DummyImplicit): (B, B)
 }
 
-object StarterPositionInstances {
-  implicit val prePre: StarterPosition[PrePlayer, Player] = new StarterPosition[PrePlayer, Player] {
-    override def random1(player1: PrePlayer, player2: PrePlayer): (PrePlayer, Player) = {
-      val token1 = List(nextInt(3) + 2, nextInt(3) + 2)
-      var token2 = List(nextInt(3) + 2, nextInt(3) + 2)
-      while (token1 == token2) {
-        token2 = List(nextInt(3) + 2, nextInt(3) + 2)
-      }
-      (player2, Player(List(token1, token2), player1.card))
+object StarterPosition extends StarterPosition[PrePlayer, Player] {
+  def random(player1: PrePlayer, player2: PrePlayer): (PrePlayer, Player) = {
+    val token1 = List(nextInt(3) + 2, nextInt(3) + 2)
+    var token2 = List(nextInt(3) + 2, nextInt(3) + 2)
+    while (token1 == token2) {
+      token2 = List(nextInt(3) + 2, nextInt(3) + 2)
     }
+    (player2, Player(List(token1, token2), player1.card))
+  }
 
-    override def random2(player1: PrePlayer, player2: Player): (Player, Player) = {
-      val token1 = List(nextInt(3) + 2, nextInt(3) + 2)
-      var token2 = List(nextInt(3) + 2, nextInt(3) + 2)
-      while (token1 == token2) {
-        token2 = List(nextInt(3) + 2, nextInt(3) + 2)
-      }
-      (player2, Player(List(token1, token2), player1.card))
+  def random(player1: PrePlayer, player2: Player)(implicit di: DummyImplicit): (Player, Player) = {
+    val token1 = List(nextInt(3) + 2, nextInt(3) + 2)
+    var token2 = List(nextInt(3) + 2, nextInt(3) + 2)
+    while (token1 == token2) {
+      token2 = List(nextInt(3) + 2, nextInt(3) + 2)
     }
-  }
-}
-
-object StarterPosition {
-  def random1[A, B](player1: A, player2: A)(implicit starterPosition: StarterPosition[A, B]) = {
-    starterPosition.random1(player1, player2)
-  }
-  def random2[A, B](player1: A, player2: B)(implicit starterPosition: StarterPosition[A, B]) = {
-    starterPosition.random2(player1, player2)
+    (player2, Player(List(token1, token2), player1.card))
   }
 }
