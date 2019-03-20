164b4cb6148a5e2f092e3b027582ec1ea37a8620
chien-wei <chien-wei@outlook.com>
2019-03-20 13:26:46 -0600

    turn checker read from console stdin

--- src/main/scala/cs6963/santorini/TurnCheck.scala
+++ src/main/scala/cs6963/santorini/TurnCheck.scala
@@ -4,15 +4,20 @@ import scala.io.Source
 object TurnCheck {
 
   def main(args: Array[String]): Unit = {
-    val filename = scala.io.StdIn.readLine() // "testcases.txt"
+    //val filename = scala.io.StdIn.readLine() // "testcases.txt"
 
     var description = ""
     var str1 = ""
     var str2 = ""
-    for ((line, i) <- Source.fromFile(filename).getLines.zipWithIndex) {
-      if (i % 3 == 0) description = line
-      else if (i % 3 == 1) str1 = line
-      else if (i % 3 == 2) {
+    var line = ""
+    var i = 0
+    //for ((line, i) <- Source.fromFile(filename).getLines.zipWithIndex) {
+    while (true) {
+      line = scala.io.StdIn.readLine()
+      if (line == "") sys.exit
+      else if (i == 0) description = line
+      else if (i == 1) str1 = line
+      else if (i == 2) {
         str2 = line
         val board1 = JSON.parseJSON(str1)
         val board2 = JSON.parseJSON(str2)
@@ -20,6 +25,8 @@ object TurnCheck {
         if (Cards.TurnCheck(board1, board2)) println("ok")
         else println("invalid")
       }
+      i += 1
+      i = i % 3
     }
   }
 }
